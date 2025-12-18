54aa62e1beb1162209f7e904490cb8b779006b22
Theodore Grist <theodoreg23456@gmail.com>
2025-12-18 16:25:55 +0100

    Snapping working

--- Hide and seek/Assets/Scripts/Snapping.cs
+++ Hide and seek/Assets/Scripts/Snapping.cs
@@ -3,16 +3,20 @@ using UnityEngine;
 public class Snapping : MonoBehaviour
 {
     LayerMask mask;
+    [SerializeField] Transform Body;
+    [SerializeField] GameObject Foot;
+    float footSpacing;
     private void Awake()
     {
+        footSpacing = this.transform.localPosition.x;
         mask = LayerMask.GetMask("Floor");
     }
     private void Update()
     {
-        Ray ray = new Ray(transform.position, Vector3.down);
+        Ray ray = new Ray (Body.position + (Body.right * footSpacing), Vector3.down);
         if(Physics.Raycast(ray, out RaycastHit hitInfo, 1, mask))
         {
-            transform.position =new Vector3(hitInfo.point.x, hitInfo.point.y + 0.01f, hitInfo.point.z);
+            transform.position = new Vector3(hitInfo.point.x, hitInfo.point.y + 0.046f, hitInfo.point.z);
         }
     }
 }
